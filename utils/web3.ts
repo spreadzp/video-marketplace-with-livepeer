228ff8c84662167a84d070dce62e5029a6fa569f
Paul K <pavlo.k@blaize.tech>
2023-04-05 19:20:11 +0300

    add new version livepeer

--- utils/web3.ts
+++ utils/web3.ts
@@ -13,16 +13,11 @@ export async function getWeb3Instance(): Promise<Web3InstanceProps> {
         const networkId = await web3.eth.net.getId()
         const accounts = await web3.eth.getAccounts();
         const currentAddress = accounts[0]    
-        let marketPlaceContract = null;
-    
+        let marketPlaceContract = null;    
         let encodedNftContract = null;
         // Get all listed NFTs
         const deployedId = Object.keys(EncodedNft.networks)[0]
-        if (networkId - +deployedId !== 0) {
-            //if (networkId !== +deployedId) {
-            // throw Error(`it works for only Mantle network 
-            // now current networkId ${networkId}`)
-      
+        if (networkId - +deployedId !== 0) { 
             window.ethereum.request({
               method: 'wallet_switchEthereumChain',
               params: [{ chainId: web3.utils.toHex(deployedId) }]
@@ -30,8 +25,7 @@ export async function getWeb3Instance(): Promise<Web3InstanceProps> {
                 marketPlaceContract = new web3.eth.Contract(Marketplace.abi as any, Marketplace.networks[`${networkId}` as keyof typeof Marketplace.networks]['address'])
     
                 encodedNftContract = new web3.eth.Contract(EncodedNft.abi as any, EncodedNft.networks[`${networkId}` as keyof typeof EncodedNft.networks]['address'])    
-            }).catch((err: any) => {
-              console.log('@@@@@@@@@@@@@@@@@err', err)
+            }).catch((err: any) => { 
               if (err.code === 4902) {
                 console.log("🚀 ~ file: web3.ts:49 ~ getWeb3Instance ~ err.code:", err.code)
                 window.ethereum.request({
@@ -40,9 +34,9 @@ export async function getWeb3Instance(): Promise<Web3InstanceProps> {
                   params: [
                     {
                       chainName: 'Localhost 8545',
-                      chainId: web3.utils.toHex(3141),
-                      nativeCurrency: { name: 'tFIL', decimals: 18, symbol: 'tFIL' },
-                      rpcUrls: ['https://api.hyperspace.node.glif.io/rpc/v1/']
+                      chainId: web3.utils.toHex(1337),
+                      nativeCurrency: { name: 'ETH', decimals: 18, symbol: 'ETH' },
+                      // rpcUrls: ['https://api.hyperspace.node.glif.io/rpc/v1/']
                     }],
                   // params: [
                   //   {
@@ -52,14 +46,7 @@ export async function getWeb3Instance(): Promise<Web3InstanceProps> {
                   //     rpcUrls: ['http://localhost:8545/']
                   //   }
                   // ],
-                  // params: [
-                  //   {
-                  //     chainName: 'Mantle Testnet',
-                  //     chainId: web3.utils.toHex(deployedId),
-                  //     nativeCurrency: { name: 'BIT', decimals: 18, symbol: 'BIT' },
-                  //     rpcUrls: ['https://rpc.testnet.mantle.xyz/']
-                  //   }
-                  // ],
+                
                   id: 0
       
                 }).then(() => {
